1b36b90a4790fe0ebf39dad7f7235280582b047c
Haitao Li <jameslihaitao1023@gmail.com>
2016-09-26 22:25:48 +1000

    Changed generic types to Object

--- database/MiniCassandra.java
+++ database/MiniCassandra.java
@@ -2,30 +2,30 @@ import java.util.*;
 
 /**
  * Cassandra is a NoSQL database, and it uses two-level keys to find a specific value.
- * In a Cassandra table, each row is determined by a raw_key (hash key / shard key).
+ * In a Cassandra columnFamily (table in RDBMS), each row is determined by a raw_key (hash key / shard key).
  * Each row can have expandable number of columns, columns are sorted by column keys and support
  * range query. Column keys are usually ids or timestamp + id.
  * Created by Haitao (James) Li on 11/09/2016.
  */
 
-public class MiniCassandra<K, S, V> {
+public class MiniCassandra {
 
-    private class Column<K, V> {
-        K key;
-        V value;
+    private class Column {
+        Object key;
+        Object value;
 
-        Column(K key, V value) {
+        Column(Object key, Object value) {
             this.key = key;
             this.value = value;
         }
 
     }
 
-    // we choose NavigableMap interface because we need to support range query on column keys
-    private Map<K, NavigableMap<S, V>> table;
+    // we choose NavigableMap interface to support range query on column keys
+    private Map<Object, NavigableMap<Object, Object>> columnFamily;
 
     public MiniCassandra() {
-        table = new HashMap<>();
+        columnFamily = new HashMap<>();
     }
 
     /**
@@ -33,11 +33,11 @@ public class MiniCassandra<K, S, V> {
      * @param column_key column key
      * @param column_value column value
      */
-    public void insert(K raw_key, S column_key, V column_value) {
-        if (!table.containsKey(raw_key)) {
-            table.put(raw_key, new TreeMap<>());
+    public void insert(Object raw_key, Object column_key, Object column_value) {
+        if (!columnFamily.containsKey(raw_key)) {
+            columnFamily.put(raw_key, new TreeMap<>());
         }
-        table.get(raw_key).put(column_key, column_value);
+        columnFamily.get(raw_key).put(column_key, column_value);
     }
 
     /**
@@ -47,15 +47,24 @@ public class MiniCassandra<K, S, V> {
      * @param column_end upper bound of column key
      * @return a list of Columns
      */
-    public List<Column<S,V>> query(K raw_key, S column_start, S column_end) {
-        List<Column<S,V>> result = new ArrayList<>();
-        if (!table.containsKey(raw_key)) {
+    public List<Column> query(Object raw_key, Object column_start, Object column_end) {
+        List<Column> result = new ArrayList<>();
+        if (!columnFamily.containsKey(raw_key)) {
             return result;
         }
-        for (Map.Entry<S, V> entry:
-                table.get(raw_key).subMap(column_start, true, column_end, true).entrySet()) {
-            result.add(new Column<>(entry.getKey(), entry.getValue()));
+        for (Map.Entry<Object, Object> entry:
+                columnFamily.get(raw_key).subMap(column_start, true, column_end, true).entrySet()) {
+            result.add(new Column(entry.getKey(), entry.getValue()));
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        MiniCassandra c = new MiniCassandra();
+        c.insert("google", 1, "haha");
+        for (Column col: c.query("google", 0, 1)) {
+            System.out.println(col.key + ": " + col.value);
+        }
+
     }
 }
